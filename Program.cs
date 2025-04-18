ebd9e71db1fbc08af8cda2392f4fd4ce01134ddf
ayush15510 <24xCheats69@gmail.com>
2025-04-18 16:46:27 +0545

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Try1.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR()
+    .AddNewtonsoftJsonProtocol();
+
+
+// 👉 Force the port to 5000
+builder.WebHost.UseUrls("http://localhost:5050"); // or 5000
+
+
+var app = builder.Build();
+app.MapHub<ChatHub>("/chathub");
+app.Run();
